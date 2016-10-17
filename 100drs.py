f91414434401212ac941eb42758255c5ac707858
Bence Gál <Bence@Bence-MacBook-Pro.local>
2016-10-17 14:36:15 +0200

    uploading

---
+++ 100drs.py
@@ -0,0 +1,19 @@
+import string
+
+
+szamok_list = ['close']*100
+
+
+for n in range (1,100):
+    
+    for j in range(n,100,n):
+    
+        if szamok_list[j] == 'close':
+            szamok_list[j] = 'open'
+        elif szamok_list[j] == 'open':
+            szamok_list[j] = 'close'
+
+for x in range(1,100):
+        if szamok_list[x] == 'open':
+            print('Door',(x)'is open :)')
+
